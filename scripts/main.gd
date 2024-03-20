026584d94959383cbe9a78d7c3c3ba383ec020b3
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2024-03-20 20:17:39 -0300

    Add laser pointer

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,5 +1,20 @@
 extends Node3D
 
+@onready var _menu := $XROrigin3D/LeftController/Menu
+@onready var _pointer := $XROrigin3D/RightController/FunctionPointer
+
+
+var _menu_enabled: bool:
+	get:
+		return _menu.visible
+	set(value):
+		_menu.visible = value
+		_menu.enabled = value
+		
+		_pointer.visible = value
+		_pointer.enabled = value
+
+
 func _ready() -> void:
 	var xr_interface := XRServer.find_interface("OpenXR")
 	
@@ -8,7 +23,12 @@ func _ready() -> void:
 		get_viewport().use_xr = true
 	else:
 		printerr("OpenXR not initialized.")
+	
+	
+	
+	_menu_enabled = false
 
 
-func _on_menu_gui_quit_button_up() -> void:
-	get_tree().quit()
+func _on_left_controller_button_pressed(button_name: String) -> void:
+	if button_name == "ax_button":
+		_menu_enabled = not _menu_enabled

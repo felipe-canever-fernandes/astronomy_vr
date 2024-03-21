6b846621a41515a5826ee79c59cc02a7ff5ddedf
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2024-03-20 22:03:32 -0300

    Add label to body

--- scripts/body.gd
+++ scripts/body.gd
@@ -2,10 +2,14 @@
 extends MeshInstance3D
 class_name Body
 
+## The name of this body that will be displayed on its label.
+@export var body_name: String
 ## The body around which this body orbits.
 @export var parent: Node3D
 ## The time it takes for this body to orbit around its parent, in seconds.
 @export var orbital_period: float = 30
+
+@onready var _label := $Label
 
 var _distance: float = 0
 var _angle: float = 0
@@ -16,6 +20,7 @@ var _angular_speed: float:
 
 
 func _ready() -> void:
+	_set_up_label()
 	_set_up_orbiting()
 
 
@@ -35,6 +40,13 @@ func _process(delta: float) -> void:
 	global_position.z = parent.global_position.z + _distance * sin(_angle)
 
 
+func _set_up_label() -> void:
+	_label.text = body_name
+	
+	var y_diameter := get_aabb().size.y
+	_label.position.y = y_diameter / 2 + 0.05
+	
+	
 func _set_up_orbiting() -> void:
 	if parent == null:
 		return

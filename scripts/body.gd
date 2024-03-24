566205af4a50386740b1aa949b02f70118ebce60
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2024-03-24 20:47:35 -0300

    Remove body description pane

--- scripts/body.gd
+++ scripts/body.gd
@@ -1,21 +1,13 @@
 extends Node3D
 class_name Body
 
-const _MAXIMUM_DESCRIPTION_PANEL_ROTATION_OFFSET := PI / 2
 const _GUI_OFFSET := 0.05
-const _AREA_OFFSET := 1
 
 @onready var _name_label: Label3D = $NameLabel
-@onready var _description_panel: Node3D = $DescriptionPanel
-@onready var _description_panel_panel: Node3D = $DescriptionPanel/Panel
-@onready var _description_panel_gui: BodyInfoGUI = _description_panel_panel.scene_node
-@onready var _area_collision_shape: CollisionShape3D = $Area/CollisionShape
 
 
 ## The name of this body that will be displayed on its label.
 @export var body_name: String
-## A description that will be shown on the description panel.
-@export_multiline var description: String
 ## The body around which this body orbits.
 @export var parent: Node3D
 ## The time it takes for this body to orbit around its parent, in seconds.
@@ -24,7 +16,6 @@ const _AREA_OFFSET := 1
 @export var rotation_period: float
 
 var _mesh: MeshInstance3D
-var _shape: SphereShape3D
 
 
 var _size: Vector3:
@@ -55,19 +46,14 @@ var _rotation_angular_speed: float:
 
 func _ready() -> void:
 	_name_label.text = body_name
-	_description_panel.visible = false
-	_description_panel_gui.text = description
 	_set_initial_orbital_distance()
 	_find_mesh()
-	_create_area_shape()
 	_set_up_orbit()
 
 
 func _process(delta: float) -> void:
 	_mesh.scale = Game.simulation_scale * Vector3.ONE
-	_shape.radius = _size.x / 2 + _AREA_OFFSET
 	_update_gui()
-	_set_description_panel_rotation()
 	_rotate(delta)
 	_orbit(delta)
 
@@ -86,17 +72,8 @@ func _find_mesh():
 			break
 
 
-func _create_area_shape() -> void:
-	_shape = SphereShape3D.new()
-	_area_collision_shape.shape = _shape
-
-
 func _update_gui() -> void:
 	_name_label.position.y = _size.y / 2 + _GUI_OFFSET
-
-	_description_panel_panel.position.x = \
-			_size.x / 2 \
-			+ _description_panel_panel.screen_size.x / 2 + _GUI_OFFSET
 	
 	
 func _set_up_orbit() -> void:
@@ -104,21 +81,6 @@ func _set_up_orbit() -> void:
 		return
 
 	_orbital_angle = acos(global_position.x / _orbital_distance)
-
-
-func _set_description_panel_rotation() -> void:
-	var player_horizontal_position := Vector3(
-		Game.player_camera_position.x,
-		_description_panel.global_position.y,
-		Game.player_camera_position.z
-	)
-
-	_description_panel.look_at(player_horizontal_position)
-
-	var offset := _MAXIMUM_DESCRIPTION_PANEL_ROTATION_OFFSET
-
-	_description_panel.global_rotation.y += \
-			-1 / exp(_size.x / 2 - log(offset)) + offset
 
 
 func _rotate(delta: float) -> void:
@@ -142,11 +104,3 @@ func _orbit(delta: float) -> void:
 
 	global_position.z = \
 			parent.global_position.z + _orbital_distance * sin(_orbital_angle)
-
-
-func _on_area_body_entered(_body: Node3D) -> void:
-	_description_panel.visible = true
-
-
-func _on_area_body_exited(_body: Node3D) -> void:
-	_description_panel.visible = false

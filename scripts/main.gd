fefe00943400c65cf16b93dd2255778d1263605c
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2024-03-24 12:34:03 -0300

    Make system start at a position relative to the player

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,8 +1,11 @@
 extends Node3D
 
-@onready var _camera := $XROrigin3D/XRCamera3D
-@onready var _menu := $XROrigin3D/LeftController/Menu
-@onready var _pointer := $XROrigin3D/RightController/FunctionPointer
+const _SYSTEM_HEIGHT_OFFSET := -0.3
+
+@onready var _camera: XRCamera3D = $XROrigin3D/XRCamera3D
+@onready var _menu: Node3D = $XROrigin3D/LeftController/Menu
+@onready var _pointer: XRToolsFunctionPointer = $XROrigin3D/RightController/FunctionPointer
+@onready var _system: Node3D = $System
 
 var _is_game_paused: bool = false
 var _old_simulation_speed_factor: float
@@ -20,6 +23,17 @@ var _menu_enabled: bool:
 
 
 func _ready() -> void:
+	_set_up_xr()
+	_set_up_controls()
+	_set_up_system()
+	_menu_enabled = false
+
+
+func _process(_delta: float) -> void:
+	Game.player_camera_position = _camera.global_position
+
+
+func _set_up_xr() -> void:
 	var xr_interface := XRServer.find_interface("OpenXR")
 	
 	if xr_interface != null and xr_interface.is_initialized():
@@ -27,18 +41,18 @@ func _ready() -> void:
 		get_viewport().use_xr = true
 	else:
 		printerr("OpenXR not initialized.")
-	
+
+
+func _set_up_controls() -> void:
 	_menu.connect_scene_signal("play_up", _on_menu_gui_play_button_up)
 	_menu.connect_scene_signal("normal_speed_up", _on_menu_gui_normal_speed_button_up)
 	_menu.connect_scene_signal("quit_button_up", _on_menu_gui_quit_button_up)
 	_menu.connect_scene_signal("increase_simulation_speed_up", _on_menu_gui_increase_simulation_speed_up)
 	_menu.connect_scene_signal("decrease_simulation_speed_up", _on_menu_gui_decrease_simulation_speed_up)
 
-	_menu_enabled = false
 
-
-func _process(_delta: float) -> void:
-	Game.player_camera_position = _camera.global_position
+func _set_up_system() -> void:
+	_system.position.y = _camera.global_position.y + _SYSTEM_HEIGHT_OFFSET
 
 
 func _on_left_controller_button_pressed(button_name: String) -> void:

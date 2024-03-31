aa2e454b61cbfde2a4fb2bb12f74112e490fda39
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2024-03-31 17:13:05 -0300

    Set sun light size

--- scripts/sun_light.gd
+++ scripts/sun_light.gd
@@ -2,7 +2,9 @@ extends OmniLight3D
 
 @onready var _initial_range: float = omni_range
 @onready var _initial_energy: float = light_energy
+@onready var _initial_size: float = light_size
 
 func _process(_delta: float) -> void:
 	omni_range = _initial_range * Game.simulation_scale
 	light_energy = _initial_energy * Game.simulation_scale
+	light_size = _initial_size * Game.simulation_scale

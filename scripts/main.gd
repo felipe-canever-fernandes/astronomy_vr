6fc132a60cbb49c4045a2dd81e80bf149eb5b949
Felipe Canever Fernandes <felipe.canever.fernandes@outlook.com>
2024-04-03 20:59:26 -0300

    Add controller button for enabling passthrough

--- scripts/main.gd
+++ scripts/main.gd
@@ -235,6 +235,8 @@ func _update_pointer_enabled() -> void:
 
 func _on_left_controller_button_pressed(button_name: String) -> void:
 	match button_name:
+		"ax_button":
+			_passthrough_enabled = not _passthrough_enabled
 		"by_button":
 			_is_menu_enabled = not _is_menu_enabled
 		"trigger_click":
